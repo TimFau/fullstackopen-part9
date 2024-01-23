c01f4fcbf49385c4c72e057f9567bda95239109c
Tim Fau <dev@timfau.com>
2024-01-22 22:00:56 -0500

    Exercise 9.1
    # "Write a function calculateBmi that calculates a BMI based on a given height (in centimeters) and weight (in kilograms) and then returns a message that suits the results."

---
+++ bmiCalculator.ts
@@ -0,0 +1,18 @@
+const calculateBmi = (height: number, weight: number) => {
+    const result = Number(((weight)  / (height * height) * 10000).toFixed(1))
+    if (result < 18.5) {
+        return "Underweight"
+    }
+    if (result <= 25) {
+        return "Normal (healthy weight)"
+    }
+    if (result < 30) {
+        return "Overweight"
+    }
+    return "Obese"
+}
+
+const height: number = Number(process.argv[2])
+const weight: number = Number(process.argv[3])
+
+console.log(calculateBmi(height, weight))
