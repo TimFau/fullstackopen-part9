68e127423eb8639a6902b517389a382216105fa3
Tim Fau <dev@timfau.com>
2024-01-26 21:18:14 -0500

    Exercise 9.5
    # "Add an endpoint for the BMI calculator that can be used by doing an HTTP GET request to the endpoint bmi and specifying the input with query string parameters."

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,4 +1,7 @@
 const calculateBmi = (height: number, weight: number) => {
+    if (Number.isNaN(height) || Number.isNaN(weight)) {
+        return  "Error: Must provide a height and weight"
+    }
     const result = Number(((weight)  / (height * height) * 10000).toFixed(1))
     if (result < 18.5) {
         return "Underweight"
@@ -16,3 +19,5 @@ const height: number = Number(process.argv[2])
 const weight: number = Number(process.argv[3])
 
 console.log(calculateBmi(height, weight))
+
+export default calculateBmi
--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+    const { height, weight } = req.query
+    const result = calculateBmi(Number(height), Number(weight))
+    if (result.includes("Error")) {
+        res.status(400)
+    }
+    res.send(result)
 })
 
 const PORT = 3003
