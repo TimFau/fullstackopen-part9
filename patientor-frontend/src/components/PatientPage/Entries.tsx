ea4bd82acc5134bb7da727a0608f9269690695c9
Tim Fau <dev@timfau.com>
2024-03-01 23:51:25 -0500

    Exercise 9.25

---
+++ patientor-frontend/src/components/PatientPage/Entries.tsx
@@ -0,0 +1,87 @@
+import { Card, CardContent } from "@mui/material";
+import {
+    Work as WorkIcon,
+    MedicalServices as MedicalServicesIcon,
+    LocalHospital as LocalHospitalIcon,
+    Favorite as HeartIcon
+} from "@mui/icons-material";
+import { Diagnosis, Entry } from "../../types";
+
+interface EntryBaseProps {
+    entry: Entry;
+}
+
+interface HospitalEntryProps extends EntryBaseProps {
+    diagnoses: Diagnosis[];
+}
+
+export const HospitalEntry = ({ entry, diagnoses }: HospitalEntryProps) => {
+    console.log('HospitalEntry', entry);
+    if ('discharge' in entry) {
+        return (
+            <Card key={entry.id} sx={{ marginBottom: '15px' }}>
+                <CardContent>
+                    <strong>{entry.date} <LocalHospitalIcon /></strong>
+                    <p>{entry.description}</p>
+                    <p>Discharged {entry.discharge?.date} - {entry.discharge?.criteria}</p>
+                    <p>Diagnose by {entry.specialist}</p>
+                    {'diagnosisCodes' in entry &&
+                        <>
+                        <h4>Diagnosis Codes</h4>
+                        <ul>
+                            {entry.diagnosisCodes?.map(code => (
+                                <li key={code}>{code} - {diagnoses?.find((diagnosis: Diagnosis) => diagnosis.code === code)?.name}</li>
+                            ))}
+                        </ul>
+                        </>
+    
+                    }
+                </CardContent>
+            </Card>
+        );
+    }
+};
+
+export const OccupationalEntry = ({ entry }: EntryBaseProps) => {
+    console.log('OccupationalEntry', entry);
+    if ('employerName' in entry) {
+        return(
+            <Card key={entry.id} sx={{ marginBottom: '15px' }}>
+                <CardContent>
+                    <strong>{entry.date} <WorkIcon /> {entry.employerName}</strong>
+                    <p>{entry.description}</p>
+                    <p>Diagnose by {entry.specialist}</p>
+                </CardContent>
+            </Card>
+        );
+    }
+};
+
+export const HealthCheckEntry = ({ entry }: EntryBaseProps) => {
+    console.log('HealthCheckEntry', entry);
+    if ('healthCheckRating' in entry) {
+        const healthCheckRatingColor = () => {
+            switch (entry.healthCheckRating) {
+            case 0:
+                return 'success';
+            case 1:
+                return 'info';
+            case 2:
+                return 'warning';
+            case 3:
+            default:
+                return 'error';
+            }
+        };
+        return (
+            <Card key={entry.id} sx={{ marginBottom: '15px' }}>
+                <CardContent>
+                    <strong>{entry.date} <MedicalServicesIcon /></strong>
+                    <p>{entry.description}</p>
+                    <HeartIcon color={healthCheckRatingColor()} />
+                    <p>Diagnose by {entry.specialist}</p>
+                </CardContent>
+            </Card>
+        );
+    }
+};
--- patientor-frontend/src/components/PatientPage/index.tsx
+++ patientor-frontend/src/components/PatientPage/index.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import patientService from "../../services/patients";
 import diagnosesService from "../../services/diagnoses";
 import { useParams } from "react-router-dom";
 import { Patient, Entry, Diagnosis } from "./../../types";
+import { HealthCheckEntry, HospitalEntry, OccupationalEntry } from "./Entries";
+
 import { Male as MaleIcon, Female as FemaleIcon } from '@mui/icons-material';
-import { Card, CardContent } from "@mui/material";
 
 const PatientPage = () => {
     const [patient, setPatient] = useState<Patient>();
-    const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
+    const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
     const { id } = useParams();
 
     useEffect(() => {
@@ -21,6 +22,19 @@ const PatientPage = () => {
         fetchPatient();
     }, [id]);
 
+    const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+        switch (entry.type) {
+            case "Hospital":
+                return <HospitalEntry entry={entry} diagnoses={diagnoses} />;
+            case "OccupationalHealthcare":
+                return <OccupationalEntry entry={entry} />;
+            case "HealthCheck":
+                return <HealthCheckEntry entry={entry} />;
+            default:
+                return null;
+        }
+    };
+
     if (!patient) {
         return <div>Loading...</div>;
     }
@@ -31,23 +45,7 @@ const PatientPage = () => {
             <p>Occupation: {patient.occupation}</p>
             <h2>Entries</h2>
             {Array.isArray(patient.entries) && patient.entries.map((entry: Entry) => (
-                <Card key={entry.id} sx={{ marginBottom: '15px' }}>
-                    <CardContent>
-                        <strong>{entry.date}</strong>
-                        <p>{entry.description}</p>
-                        {'diagnosisCodes' in entry &&
-                            <>
-                            <h4>Diagnosis Codes</h4>
-                            <ul>
-                                {entry.diagnosisCodes?.map(code => (
-                                    <li key={code}>{code} - {diagnoses?.find((diagnosis: Diagnosis) => diagnosis.code === code)?.name}</li>
-                                ))}
-                            </ul>
-                            </>
-
-                        }
-                    </CardContent>
-                </Card>
+                <EntryDetails entry={entry} key={entry.id} />
             ))}
         </>
     );
